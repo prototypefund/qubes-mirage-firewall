6cf6fa9a43b2533d1d4c2333b0239ec9c45e2636
Mindy <meetup@yomimono.org>
2019-02-27 04:50:17 -0600

    uplink is no longer a functor

--- unikernel.ml
+++ unikernel.ml
@@ -8,7 +8,6 @@ let src = Logs.Src.create "unikernel" ~doc:"Main unikernel code"
 module Log = (val Logs.src_log src : Logs.LOG)
 
 module Main (Clock : Mirage_clock_lwt.MCLOCK) = struct
-  module Uplink = Uplink.Make
 
   (* Set up networking and listen for incoming packets. *)
   let network nat qubesDB =
--- uplink.ml
+++ uplink.ml
@@ -9,64 +9,62 @@ module Eth = Ethernet.Make(Netif)
 let src = Logs.Src.create "uplink" ~doc:"Network connection to NetVM"
 module Log = (val Logs.src_log src : Logs.LOG)
 
-module Make = struct
-  module Arp = Arp.Make(Eth)(OS.Time)
+module Arp = Arp.Make(Eth)(OS.Time)
 
-  type t = {
-    net : Netif.t;
-    eth : Eth.t;
-    arp : Arp.t;
-    interface : interface;
-  }
+type t = {
+  net : Netif.t;
+  eth : Eth.t;
+  arp : Arp.t;
+  interface : interface;
+}
 
-  class netvm_iface eth mac ~my_ip ~other_ip : interface = object
-    val queue = FrameQ.create (Ipaddr.V4.to_string other_ip)
-    method my_mac = Eth.mac eth
-    method my_ip = my_ip
-    method other_ip = other_ip
-    method writev ethertype fillfn =
-      FrameQ.send queue (fun () ->
+class netvm_iface eth mac ~my_ip ~other_ip : interface = object
+  val queue = FrameQ.create (Ipaddr.V4.to_string other_ip)
+  method my_mac = Eth.mac eth
+  method my_ip = my_ip
+  method other_ip = other_ip
+  method writev ethertype fillfn =
+    FrameQ.send queue (fun () ->
         mac >>= fun dst ->
         Eth.write eth dst ethertype fillfn >|= or_raise "Write to uplink" Eth.pp_error
       )
-  end
-
-  let listen t router =
-    (* TODO: this header size is not meaningful??? *)
-    Netif.listen t.net ~header_size:0 (fun frame ->
-        (* Handle one Ethernet frame from NetVM *)
-        Eth.input t.eth
-          ~arpv4:(Arp.input t.arp)
-          ~ipv4:(fun ip ->
-              match Nat_packet.of_ipv4_packet ip with
-              | exception ex ->
-                Log.err (fun f -> f "Error unmarshalling ethernet frame from uplink: %s@.%a" (Printexc.to_string ex)
-                            Cstruct.hexdump_pp frame
-                        );
-                Lwt.return_unit
-              | Error e ->
-                Log.warn (fun f -> f "Ignored unknown IPv4 message from uplink: %a" Nat_packet.pp_error e);
-                Lwt.return ()
-              | Ok packet ->
-                Firewall.ipv4_from_netvm router packet
-            )
-          ~ipv6:(fun _ip -> return ())
-          frame
-      ) >|= or_raise "Uplink listen loop" Netif.pp_error
-
-  let interface t = t.interface
-
-  let connect config =
-    let ip = config.Dao.uplink_our_ip in
-    Netif.connect "0" >>= fun net ->
-    Eth.connect net >>= fun eth ->
-    Arp.connect eth >>= fun arp ->
-    Arp.add_ip arp ip >>= fun () ->
-    let netvm_mac =
-      Arp.query arp config.Dao.uplink_netvm_ip
-      >|= or_raise "Getting MAC of our NetVM" Arp.pp_error in
-    let interface = new netvm_iface eth netvm_mac
-      ~my_ip:ip
-      ~other_ip:config.Dao.uplink_netvm_ip in
-    return { net; eth; arp; interface }
 end
+
+let listen t router =
+  (* TODO: this header size is not meaningful??? *)
+  Netif.listen t.net ~header_size:0 (fun frame ->
+      (* Handle one Ethernet frame from NetVM *)
+      Eth.input t.eth
+        ~arpv4:(Arp.input t.arp)
+        ~ipv4:(fun ip ->
+            match Nat_packet.of_ipv4_packet ip with
+            | exception ex ->
+              Log.err (fun f -> f "Error unmarshalling ethernet frame from uplink: %s@.%a" (Printexc.to_string ex)
+                          Cstruct.hexdump_pp frame
+                      );
+              Lwt.return_unit
+            | Error e ->
+              Log.warn (fun f -> f "Ignored unknown IPv4 message from uplink: %a" Nat_packet.pp_error e);
+              Lwt.return ()
+            | Ok packet ->
+              Firewall.ipv4_from_netvm router packet
+          )
+        ~ipv6:(fun _ip -> return ())
+        frame
+    ) >|= or_raise "Uplink listen loop" Netif.pp_error
+
+let interface t = t.interface
+
+let connect config =
+  let ip = config.Dao.uplink_our_ip in
+  Netif.connect "0" >>= fun net ->
+  Eth.connect net >>= fun eth ->
+  Arp.connect eth >>= fun arp ->
+  Arp.add_ip arp ip >>= fun () ->
+  let netvm_mac =
+    Arp.query arp config.Dao.uplink_netvm_ip
+    >|= or_raise "Getting MAC of our NetVM" Arp.pp_error in
+  let interface = new netvm_iface eth netvm_mac
+    ~my_ip:ip
+    ~other_ip:config.Dao.uplink_netvm_ip in
+  return { net; eth; arp; interface }
